cd8b10101f73abcd41af212e5fc49560a132d667
Vladislav Valek <vladislav.valek@email.cz>
2025-07-26 19:51:58 +0200

    feat(mfb-speed-meter): add improved DT generator
    The previously used procedure did not allow to change the vendor in the
    compatible string and its form of writing is discouraged. Therefore, a
    new procedure has been created that can be more dynamically adjusted.

--- comp/mfb_tools/logic/speed_meter/DevTree.tcl
+++ comp/mfb_tools/logic/speed_meter/DevTree.tcl
@@ -1,3 +1,10 @@
+# DevTree.tcl: script containg Device Tree generation procedure
+# Copyright (C) 2025 CESNET
+# Author(s): Vladislav Valek <valekv@cesnet.cz>
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+# DEPRECATED: Use more general procedure below
 proc dts_speed_meter {base {name "speed_meter"}} {
     set ret ""
     append ret "$name {"
@@ -6,4 +13,13 @@ proc dts_speed_meter {base {name "speed_meter"}} {
     append ret "reg = <$base 0x1c>;"
     append ret "};"
     return $ret
+}
+
+proc dts_speed_meter {DTS base {id 0} {name "speed_meter"} {vendor "cesnet"}} {
+    upvar 1 $DTS dts
+
+    dts_create_node dts "$name" {
+        dts_appendprop_comp_node dts $base 0x1c "$vendor,$name"
+        dts_appendprop_int dts "version" 0x00000001
+    }
 }
