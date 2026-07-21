08208e27b5c2e669faa3845c58a5eacb24145cdc
Vladislav Valek <vladislav.valek@email.cz>
2026-07-21 19:48:41 +0200

    chore(dma_iuventus): extract core RTL into the private ndk-mod-dma-iuventus submodule

--- comp/dma/dma_iuventus/comp/software_manager/comp/cqe_error_tracker/Modules.tcl
+++
@@ -1,8 +0,0 @@
-# Modules.tcl: Components include script
-# Copyright (C) 2026 CESNET
-# Author(s): Vladislav Valek <xvalek14@vutbr.cz>
-
-lappend PACKAGES "$OFM_PATH/comp/base/pkg/nvme_meta_pack.vhd"
-lappend PACKAGES "$OFM_PATH/comp/dma/dma_iuventus/pkg/iuventus_bar_map_pkg.vhd"
-
-lappend MOD "$ENTITY_BASE/cqe_error_tracker.vhd"
